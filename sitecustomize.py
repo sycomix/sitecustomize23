76a28b8c6bcd234283e3416267bdaf574470edd9
slee047 <samuellee270991@gmail.com>
2017-11-23 16:06:53 +0800

    Update sitecustomize.py
    Updated the file so that:
    
    1) It will auto create the folders per variable values if necessary.
    
    2) Python will not crash when the absence of PythonPATH, PythonPATH2 or PythonPATH3 but this file will have no effect then.
    
    3: Sets default encoding to UTF-8 for Asian users, whose directory path might not be in ASCII.
    
    4: Auto appends /Lib/site-packages to PythonPATH2 / PythonPATH3 if necesssary.

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,14 +1,30 @@
 import sys
 import os
 #Based on: https://stackoverflow.com/questions/16114391/adding-directory-to-sys-path-pythonpath
-boolPython2 = True #Assume Python 2 only.
-loadPath = os.environ['PythonPATH2']
-if (sys.version_info > (3, 0)):
-	loadPath = os.environ['PythonPATH3'] #IF Python 3.
+try:
+	loadPath = os.environ['PythonPATH2'] #Get Python 2.
+	if (sys.version_info > (3, 0)):
+		loadPath = os.environ['PythonPATH3'] #IF Python 3.
+	else:
+		sys.setdefaultencoding("utf-8") #Tune for Python 2 to default as UTF-8 (N/A for Python 3).
 
-os.environ['PythonPATH'] = loadPath
-sys.path.insert(0,loadPath)
+	os.environ['PythonPATH'] = loadPath #Current PythonPATH is only useful to load this file, and nothing else.
+	parentFolder = loadPath.split(os.sep)[-2] #The previous folder name.
+	if (parentFolder != "Lib"):
+		loadPath = os.path.join(loadPath, "Lib")
 
-#Adapted from: https://pymotw.com/2/site/
-import site
-site.addsitedir(loadPath)
+	currentFolder = loadPath.split(os.sep)[-1] #The current folder name.
+	if (currentFolder != "site-packages"):
+		loadPath = os.path.join(loadPath, "site-packages")
+
+	#Using --prefix must have a value that end with '\Lib\site-packages' regardless of OS.
+	sys.path.insert(0,loadPath) #Prioirtize this folder to load the binaries.
+
+	#Adapted from: https://pymotw.com/2/site/
+	import site
+	site.addsitedir(loadPath)
+
+	if (not os.path.isdir(loadPath)):
+		os.makedirs(loadPath) #Automatically create folder if necessary.
+except:
+	pass
